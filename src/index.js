01059df6211558cfa32551be262a187896135747
Alex Karelin <alex.karelin@yournextagency.com>
2024-08-09 21:35:05 +0700

    add /hello and /users routes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import fastify from 'fastify';
 const app = fastify();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/users', (req, res) => {
+    res.send('GET /users');
+});
+
+app.post('/users', (req, res) => {
+    res.send('POST /users');
+});
+
+app.get('/hello', (req, res) => {
+    const { user } = req.query;
+    if (user !== undefined) {
+        res.send(`Hello, ${user}!`);
+    } else {
+        res.send('Hello, World!');
+    }
 });
 
 app.listen({ port }, () => {
