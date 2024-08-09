ec9447ab634701140d356ca63d8b5be8e869dd3a
Alex Karelin <alex.karelin@yournextagency.com>
2024-08-09 22:58:12 +0700

    add nested routes

--- src/index.js
+++ src/index.js
@@ -7,8 +7,28 @@ app.get('/users', (req, res) => {
     res.send('GET /users');
 });
 
-app.post('/users', (req, res) => {
-    res.send('POST /users');
+app.get('/users/:id', (req, res) => {
+    res.send(`User ID: ${req.params.id}`);
+});
+
+app.get('/users/:userId/post/:postId', (req, res) => {
+    res.send(`User ID: ${req.params.userId}; Post ID: ${req.params.postId}`);
+});
+
+app.get('/courses', (req, res) => {
+    res.send('GET /courses');
+});
+
+app.get('/courses/new', (req, res) => {
+    res.send('Course build');
+});
+
+app.get('/courses/:id', (req, res) => {
+    res.send(`Course ID: ${req.params.id}`);
+});
+
+app.get('/courses/:courseId/lessons/:id', (req, res) => {
+    res.send(`Course ID: ${req.params.courseId}; Lesson ID: ${req.params.id}`);
 });
 
 app.get('/hello', (req, res) => {
