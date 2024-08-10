1e16843d5a6c1a452d79464d5e1e87849ce57c6b
Alex Karelin <alex.karelin@yournextagency.com>
2024-08-10 16:28:11 +0700

    add form to filter courses

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import fastify from 'fastify';
 import view from '@fastify/view';
 import pug from 'pug';
+import sanitize from 'sanitize-html';
 import generateCourses from "./utils/factories/courseFactory.js";
 
 const courses = generateCourses()
 const app = fastify();
 const port = 3000;
-
+const sanitizeOptions = {disallowedTagsMode: 'recursiveEscape'};
 await app.register(view, { engine: { pug } });
 
 app.get('/courses', (req, res) => {
-    res.view('src/views/courses/index', { courses });
+    const term = req.query.term || '';
+    const sanitizedTerm = sanitize(term, sanitizeOptions);
+    let foundCourses = courses;
+    if (term) {
+        foundCourses = courses.filter(({title, description}) => title.includes(sanitizedTerm) || description.includes(sanitizedTerm));
+    }
+
+    res.view('src/views/courses/index', { term, courses: foundCourses });
 });
 
 app.get('/courses/:id', (req, res) => {
@@ -21,6 +29,12 @@ app.get('/courses/:id', (req, res) => {
     }
 
     res.view('src/views/courses/show', course);
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = sanitize(req.params.id, sanitizeOptions);
+    res.type('html');
+    res.send(`<h1>${id}</h1>`);
 });
 
 app.listen({ port }, () => {
